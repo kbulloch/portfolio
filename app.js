f01ffcf07a16748e43cd0431b0477d00e7e40efd
Kyle Bulloch <student@epicodus.com>
2015-05-01 14:17:06 -0700

    Change index.html to use angular UI-views

---
+++ app.js
@@ -0,0 +1,21 @@
+var app = angular.module("app", ["ui.router"]);
+
+app.config(function($stateProvider) {
+  $stateProvider.state("index", {
+    url: "",
+    views: {
+      "bio": {
+        templateUrl: "partials/bio.html"
+      },
+      "work": {
+        templateUrl: "partials/work.html"
+      },
+      "education": {
+        templateUrl: "partials/education.html"
+      },
+      "contact": {
+        templateUrl: "partials/contact.html"
+      }
+    }
+  });
+});
